c2cf415e77e23b095c1531d6c9c1a7539061c975
sasisusesuso <95947346+sasisusesuso@users.noreply.github.com>
2022-07-18 03:08:51 +0900

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 # リツイートする対象検索キーワード
-SEARCH_WORD = 'from:neweconomy_g OR from:coin_post OR from:CoinDeskjapan'
+SEARCH_WORD = 'from:neweconomy_g OR from:coin_post OR from:CoinDeskjapan from:BitcoinTech5'
 
 # CMC情報
 CMC_XRP_ID = '52'
